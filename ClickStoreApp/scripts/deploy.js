80f81cb8d07f7f4d83d84696aa6b268a6e131edd
Mtorsha <torshamazumdar@gmail.com>
2022-11-16 16:44:09 -0800

    hardhat-goerli deploy

--- ClickStoreApp/scripts/deploy.js
+++ ClickStoreApp/scripts/deploy.js
@@ -4,8 +4,8 @@ async function main() {
 
   const [sender] = await ethers.getSigners();
   // console.log(sender);
-  const ClickStore = await ethers.getContractFactory("CampaignFactory");
-  // const contract = await ClickStore.deploy(0.1,sender.address,[0.1],{ value: ethers.utils.parseEther("0.1")});
+  const ClickStore = await ethers.getContractFactory("Image_NFT_MarketPlace");
+  
   const contract = await ClickStore.deploy();
   await contract.deployed();
   console.log("Contract deployed at:", contract.address);
